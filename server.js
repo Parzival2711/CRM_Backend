55dd1e0cf1ab4375c3ad55f44f3fa0a205ad4800
Parzival2711 <89346495+Parzival2711@users.noreply.github.com>
2022-12-24 23:07:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 const serverConfig = require('./configs/server.config');
 const dbConfig = require('./configs/db.config');
-const routes = require('./routes/auth.routes');
+const auth_routes = require('./routes/auth.routes');
+const user_routes = require('./routes/user.routes');
 const middlwares = require('./middlewares/auth.middleware');
 
 //Connecting to database
@@ -16,7 +17,8 @@ mongoose.connect(dbConfig.DB_URL).then(function(){
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(middlwares.isTokenValid);
-routes(app);
+auth_routes(app);//Auth Routes
+user_routes(app);//User Routes
 //Starting the server
 app.listen(serverConfig.PORT, ()=>{
     console.log("Application started on the port : "+serverConfig.PORT);
