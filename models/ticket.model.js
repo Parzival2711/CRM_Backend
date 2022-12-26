6460615a31a0b7433ec8f2c05b8499881c66621f
Parzival2711 <89346495+Parzival2711@users.noreply.github.com>
2022-12-26 23:11:46 +0530

    Add files via upload

---
+++ models/ticket.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const ticketSchema = new mongoose.Schema({
+    ticketId:{
+        type : Number,
+        length : 5,
+        unique : true,
+        required : true
+    },
+    priority:{
+        type : Number,
+        required : true,
+        default : 4
+    },
+    title:{
+        type : String,
+        required : true
+    },
+    description:{
+        type : String,
+        required : true
+    },
+    assignee:{
+        type : String
+    },
+    reporter:{
+        type : String,
+        required : true
+    },
+    status:{
+        type : String,
+        required : true,
+        default : "OPEN"
+    },
+    createdAt:{
+        type : Date,
+        immutable : true,
+        default : Date.now()
+    },
+    updatedAt:{
+        type : Date,
+        default : Date.now()
+    }
+})
+userSchema.plugin(uniqueValidator,{type:'mongoose-unique-validator'});
+module.exports = mongoose.model("Ticket",ticketSchema);
