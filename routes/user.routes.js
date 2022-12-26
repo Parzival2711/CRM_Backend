01d4304f68e1c985440ff3063ade1e5a220e4d3f
Parzival2711 <89346495+Parzival2711@users.noreply.github.com>
2022-12-26 23:12:29 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,5 +2,6 @@ module.exports = function(app){
     const userHandler = require('../controllers/user.controller');
     const userMiddleware = require('../middlewares/user.middleware');
     app.route("/crm/api/V1/admin/users").get(userMiddleware.isAdmin,userHandler.getUsers);
-    app.route('/crm/api/V1/admin/update/:userId').put(userMiddleware.isAdmin,userHandler.adminUpdate);
+    app.route('/crm/api/V1/admin/update/:userId').put(userMiddleware.isAdmin,userMiddleware.validateUpdate,userHandler.adminUpdate);
+    app.route('/crm/api/V1/user/update').put(userMiddleware.validateUpdate,userHandler.userUpdate);
 }
