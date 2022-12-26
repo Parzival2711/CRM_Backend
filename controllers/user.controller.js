2a74eea657a3f142ce9b546140947bdd671814d1
Parzival2711 <89346495+Parzival2711@users.noreply.github.com>
2022-12-26 23:09:45 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model');
 const constants = require('../utils/constants');
 const ObjectConverter = require('../utils/ObjectConverter');
+const bcrypt = require('bcrypt');
 //Fetching users based on usertype,userstatus
 exports.getUsers = async(req,res)=>{
     //Filter
@@ -52,12 +53,12 @@ exports.getUsers = async(req,res)=>{
 //update userType,userStatus & email of any user
 exports.adminUpdate = async(req,res)=>{
     let userIdparam = req.params.userId;
-    let req_status = req.userStatus;
-    let req_email = req.email;
+    let req_status = req.body.userStatus;
+    let req_email = req.body.email;
     console.log(userIdparam);
     //Approving an user
     try{
-        var user = await User.updateOne({
+        var user = await User.findOneAndUpdate({
             userId:userIdparam
         },{
             userStatus:req_status,
@@ -65,7 +66,9 @@ exports.adminUpdate = async(req,res)=>{
         }
         );
         console.log(user);
-        res.status(200).send(user);
+        res.status(200).send({
+            message:"Data has been updated successfully"
+        });
     }
     catch(err){
         console.log("Update failed for : ",userIdparam);
@@ -73,5 +76,25 @@ exports.adminUpdate = async(req,res)=>{
             message:"Some Internal error occurred"
         });
     }
-    
+}
+//Individual user's update handler for updating personal fields
+exports.userUpdate = async(req,res)=>{
+    let newPassword = bcrypt.hashSync(req.body.password,10);
+    let newContact = req.body.contact;
+    try{
+        let updatedUser = await User.findOneAndUpdate({
+            userId:req.user.userId
+        },{
+            password:newPassword,
+            contact:newContact
+        })
+        res.status(200).send({
+            message:"Data has been updated successfully"
+        })
+    }catch(err){
+        console.log("Data Couldn't be updated");
+        res.status(500).send({
+            message:"Some Internal error occurred"
+        });
+    }
 }
