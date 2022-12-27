58626c014bb1015bfb702fe9ae0c336212d31d07
Parzival2711 <89346495+Parzival2711@users.noreply.github.com>
2022-12-27 23:04:45 +0530

    Add files via upload

---
+++ routes/ticket.routes.js
@@ -0,0 +1,7 @@
+module.exports = (app)=>{
+    const ticketHandler = require('../controllers/ticket.controller');
+    const authMiddleware = require('../middlewares/auth.middleware');
+    const userMiddleware = require('../middlewares/user.middleware');
+    app.route("/crm/api/V1/ticket/create").post(authMiddleware.isLoginRequired,ticketHandler.createTicket);
+    app.route("/crm/api/V1/ticket/:ticketId").put(authMiddleware.isLoginRequired);
+}
