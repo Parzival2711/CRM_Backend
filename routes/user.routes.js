5b93d9408653d7a8183514af9b62fdcedec067d7
Parzival2711 <89346495+Parzival2711@users.noreply.github.com>
2022-12-27 23:06:09 +0530

    Update user.routes.js
    Route added for admin updation of tickets

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,9 @@
 module.exports = function(app){     
     const userHandler = require('../controllers/user.controller');
     const userMiddleware = require('../middlewares/user.middleware');
+    const ticketHandler = require('../controllers/ticket.controller');
     app.route("/crm/api/V1/admin/users").get(userMiddleware.isAdmin,userHandler.getUsers);
     app.route('/crm/api/V1/admin/update/:userId').put(userMiddleware.isAdmin,userMiddleware.validateUpdate,userHandler.adminUpdate);
     app.route('/crm/api/V1/user/update').put(userMiddleware.validateUpdate,userHandler.userUpdate);
+    app.route('/crm/api/V1/admin/ticket/update/:ticketId').put(userMiddleware.isAdmin,userMiddleware.validateUpdate,ticketHandler.updateTicketAdmin);
 }
