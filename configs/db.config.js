457c6a9eda452abed728cbaf26227a8b9686a990
Parzival2711 <89346495+Parzival2711@users.noreply.github.com>
2023-01-02 22:59:15 +0530

    Update db.config.js
    Updated with cloud URL

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     DB_NAME : "crm_database",
-    DB_URL : "mongodb://localhost:27017/crm_database"
+    DB_URL : "mongodb://localhost:27017/crm_database",
+    DB_CLOUD_URL :"mongodb+srv://abhilash97:Upgrad123@cluster0.mkkeedg.mongodb.net/crm_database"
 }
